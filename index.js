111e3494b3b2a3326a97cb10a8664e8a7a62a1e0
mrlosa <roberto@riutort.com>
2020-04-04 15:53:39 -0700

    1 file modified

--- index.js
+++ index.js
@@ -1,76 +1,59 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import PropTypes from 'prop-types';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import PropTypes from "prop-types";
+import Hello from "./Hello";
+import "./style.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React',
-      last:  'Js'
+      name: "React",
+      last: "Js"
     };
 
     // validate
     Hello.propTypes = {
-    name: PropTypes.string,
-    last: PropTypes.string.isRequired
-      }
+      name: PropTypes.string,
+      last: PropTypes.string.isRequired
+    };
+  }
 
-      
-  };
-
-  
-  
   render() {
     // Use Interpolation in JSX
     // https://egghead.io/lessons/react-understand-and-use-interpolation-in-jsx
-    function CharacterCount ({text}) {
-        return (
-          <div>
-          {`The prop name "${text}" has `} <strong>({text.length})</strong> characters
+    function CharacterCount({ text }) {
+      return (
+        <div>
+          {`The prop name "${text}" has `} <strong>({text.length})</strong>{" "}
+          characters
+        </div>
+      );
+    }
 
-          </div>
-        )
-      }
+    function tick() {
+      const time = new Date().toLocaleTimeString();
 
-      function tick() {
-        const time = new Date().toLocaleTimeString()
-        
-
-        return (
-          <>
-          {time}
-          
-        
-          
-          </>
-        )
-      };
+      return <>{time}</>;
+    }
 
     return (
-      
       <div>
-      {tick(setInterval(tick, 1000))}
+        {tick()}
+        {setInterval(tick, 1000)}
         {console.log(tick())}
-        <Hello name={this.state.name} last={this.state.last}/>
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        
+        <Hello name={this.state.name} last={this.state.last} />
+        <p>Start editing to see some magic happen :)</p>
 
-        <CharacterCount text= {this.state.name} />
-        <CharacterCount text= {this.state.last} />
+        <CharacterCount text={this.state.name} />
+        <CharacterCount text={this.state.last} />
 
         <br />
         {tick()}
-        
-
       </div>
     );
-    
   }
 }
 
-
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
