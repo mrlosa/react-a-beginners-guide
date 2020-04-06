0d28534a4a65847dd1124e391c13c220c82a13c9
mrlosa <roberto@riutort.com>
2020-04-06 11:23:08 -0700

    1 file modified

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ import Hello from "./Hello";
 import "./style.css";
 
 class App extends Component {
-
-  
-  
   constructor() {
-    
     super();
     this.state = {
       name: "React",
@@ -23,87 +19,104 @@ class App extends Component {
       name: PropTypes.string,
       last: PropTypes.string.isRequired,
       eMpty: PropTypes.string
-      
-      
-        
     };
-
-    
   }
 
   render() {
     // Use Interpolation in JSX
     function CharacterCount({ text }) {
-      
-      
       return (
         <div>
-          {`The prop name "${text} "has `} {text.length ? <strong style={{color:'green'}}>({text.length})</strong> : <span style={{color:'red'}}>'No' </span>}
+          {`The prop name "${text} "has `}{" "}
+          {text.length ? (
+            <strong style={{ color: "green" }}>({text.length})</strong>
+          ) : (
+            <span style={{ color: "red" }}>'No' </span>
+          )}
           characters
-          <div> This prop name is {text.length ? <strong style={{color:'green'}}>({text})</strong> : <span style={{color:'red'}}>'No name' </span>}</div>
+          <div>
+            {" "}
+            This prop name is{" "}
+            {text.length ? (
+              <strong style={{ color: "green" }}>({text})</strong>
+            ) : (
+              <span style={{ color: "red" }}>'No name' </span>
+            )}
+          </div>
         </div>
       );
     }
 
-    
-  //set time
+    //set time
     function Tick() {
       const time = new Date().toLocaleTimeString();
 
       const xtime = setInterval(() => {
-      //autoPlay some for specific period of times or
-      // Do some stuff you want
-      time
-    }, 1000);
-      
-      const x = xtime
-      console.log(x)
-      console.log(time)
-      return (<> {time} </>);
+        //autoPlay some for specific period of times or
+        // Do some stuff you want
+        time;
+      }, 1000);
+
+      const x = xtime;
+      console.log(x);
+      console.log(time);
+      return <> {time} </>;
     }
 
     // useState hook
     function Greeting() {
-      const [name, setName] = React.useState( window.localStorage.getItem('name') || '')
-      const [name2, setName2] = React.useState(window.localStorage.getItem('name2') || '')
-      const handleCahnge = event => setName(event.target.value)
-      const handleCahnge2 = event => setName2(event.target.value)
+      const [name, setName] = React.useState(
+        window.localStorage.getItem("name") || ""
+      );
+      const [name2, setName2] = React.useState(
+        window.localStorage.getItem("name2") || ""
+      );
+      const handleCahnge = event => setName(event.target.value);
+      const handleCahnge2 = event => setName2(event.target.value);
 
       React.useEffect(() => {
-        window.localStorage.setItem('name', name)
-        window.localStorage.setItem('name2', name2)
-        
-      })
+        window.localStorage.setItem("name", name);
+        window.localStorage.setItem("name2", name2);
+      });
 
       return (
         <div>
-        <h2>Form React Hook</h2>
-        <form>
-        <label htmlFor="name">Name:  </label>
-        <input value={name} onChange={handleCahnge} id="name" />
-        </form>
-        {name ?<strong>Hello {name}</strong> : <span style={{color:'red'}}>Please type your name.</span>}
+          <h2>Form React Hook</h2>
+          <form>
+            <label htmlFor="name">Name: </label>
+            <input value={name} onChange={handleCahnge} id="name" />
+          </form>
+          {name ? (
+            <strong>Hello {name}</strong>
+          ) : (
+            <span style={{ color: "red" }}>Please type your name.</span>
+          )}
 
-        <div>
-        <form>
-        <label htmlFor="name2">Last Name:  </label>
-        <input value={name2} onChange={handleCahnge2} id="name2" />
-        </form>
-        {name2 ?<strong>Hello {name2}</strong> : <span style={{color:'red'}}>Please type your last name.</span>}
-        </div>
+          <div>
+            <form>
+              <label htmlFor="name2">Last Name: </label>
+              <input value={name2} onChange={handleCahnge2} id="name2" />
+            </form>
+            {name2 ? (
+              <strong>Hello {name2}</strong>
+            ) : (
+              <span style={{ color: "red" }}>Please type your last name.</span>
+            )}
+          </div>
         </div>
       );
     }
-
-    
 
     return (
       <div>
         <Tick />
         {Tick()}
-        
-        <Hello name={this.state.name} last={this.state.last} 
-        eMpty={this.state.eMpty} />
+
+        <Hello
+          name={this.state.name}
+          last={this.state.last}
+          eMpty={this.state.eMpty}
+        />
         <br />
         <p>Start editing to see some magic happen :)</p>
 
